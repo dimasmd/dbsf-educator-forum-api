e1a86b6d9113af502f75645d2d0add49c96519ac
Dimas Saputra <dimas.maulanads@gmail.com>
2023-10-22 06:33:30 +0700

    implement add thread repository

---
+++ src/Infrastructures/repository/ThreadRepositoryPostgres.js
@@ -0,0 +1,24 @@
+const ThreadRepository = require('../../Domains/threads/ThreadRepository');
+
+class ThreadRepositoryPostgres extends ThreadRepository {
+  constructor(pool, idGenerator) {
+    super();
+    this._pool = pool;
+    this._idGenerator = idGenerator;
+  }
+
+  async addThread(newThread) {
+    const { title, body, owner } = newThread;
+    const id = `thread-${this._idGenerator()}`;
+
+    const query = {
+      text: 'INSERT INTO threads VALUES($1, $2, $3, $4, $5) RETURNING id, title, owner',
+      values: [id, title, body, owner, new Date().toISOString()],
+    };
+
+    const result = await this._pool.query(query);
+    return result.rows[0];
+  }
+}
+
+module.exports = ThreadRepositoryPostgres;
